514c3909ff4502f557505106125887e2d9c4537d
Mohan Avula <mohanavula@yahoo.com>
2020-06-16 14:45:10 +0530

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,97 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="">
+    <!--
+    <header class="bg-yellow-500">
+      <div class="flex px-4 py-3 items-center justify-between ">
+        <div class="">
+          <h2 class="text-xl text-gray-700 font-semibold">Word<span class="text-gray-900 font-bold">Z</span></h2>
+        </div>
+        <div>
+          <button @click="isOpen = !isOpen" type="buton" class="block text-white focus:text-yellow-100 focus:outline-none">
+            <svg class="ham hamRotate ham8 h-8 w-8 fill-current" viewBox="0 0 100 100"  onclick="this.classList.toggle('active')">
+              <g>
+                <path
+                      class="line top"
+                      d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+                <path
+                      class="line middle"
+                      d="m 30,50 h 40" />
+                <path
+                      class="line bottom"
+                      d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+              </g>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div :class="isOpen ? 'block' : 'hidden'" class="px-4 pb-3 text-left">
+        <a href="#" class="block text-gray-700 font-light hover:font-semibold">Easy</a>
+        <a href="#" class="block text-gray-700 font-light hover:font-semibold">Medium</a>
+        <a href="#" class="block text-gray-700 font-light hover:font-semibold">Advanced</a>
+      </div>
+    </header>
+    -->
+    <div class=" max-w-md h-auto m-auto rounded-md bg-yellow-500 p-3 mt-2">
+      Designed by Avula Mohan
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isOpen: false,
+      }
+    },
+  }
+</script>
+  
+<style scoped>
+  .ham {
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+    transition: transform 400ms;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .hamRotate.active {
+    transform: rotate(45deg);
+  }
+  .hamRotate180.active {
+    transform: rotate(180deg);
+  }
+  .line {
+    fill:none;
+    transition: stroke-dasharray 400ms, stroke-dashoffset 400ms;
+    stroke: rgb(26, 32, 44);
+    stroke-width:5.5;
+    stroke-linecap:round;
+  }
+  
+  .ham8 .top {
+    stroke-dasharray: 40 160;
+  }
+  .ham8 .middle {
+    stroke-dasharray: 40 142;
+    transform-origin: 50%;
+    transition: transform 400ms;
+  }
+  .ham8 .bottom {
+    stroke-dasharray: 40 85;
+    transform-origin: 50%;
+    transition: transform 400ms, stroke-dashoffset 400ms;
+  }
+  .ham8.active .top {
+    stroke-dashoffset: -64px;
+  }
+  .ham8.active .middle {
+    stroke-dashoffset: 0px;
+    transform: rotate(90deg);
+  }
+  .ham8.active .bottom {
+    stroke-dashoffset: -64px;
+  }
+</style>
